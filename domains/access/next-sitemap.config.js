c417afdb1bdfa6b992cdf9fed63783ce69b1289f
Gitit2meBaby <thisworldofdans@gmail.com>
2024-11-08 15:09:13 +0700

    sitemap configured

--- domains/access/next-sitemap.config.js
+++ domains/access/next-sitemap.config.js
@@ -1,4 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://accessexperts.com.au',
-    generateRobotsTxt: true,
+  siteUrl: "https://accessexperts.com.au",
+  generateRobotsTxt: true,
+  // Optional: other configuration
+  changefreq: "weekly",
+  priority: 0.7,
+  exclude: ["/api/*"],
+  generateIndexSitemap: false,
 };
